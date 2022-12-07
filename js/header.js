6a4c42ab557b6b116bf903b6a0651aee6b9b69b1
Yenhj <yen.greact@gmail.com>
2022-12-07 18:14:28 +0900

    js 작업중이요

--- js/header.js
+++ js/header.js
@@ -1,13 +1,13 @@
-// {
-//   console.log("헤더작업");
-//   const header = document.querySelector(".header");
-//   let scTop = 0;
-//   window.addEventListener("scroll", function () {
-//     scTop = document.documentElement.scrollTop;
-//     if (scTop > 0) {
-//       header.classList.add("active");
-//     } else {
-//       header.classList.remove("active");
-//     }
-//   });
-// }
+{
+  console.log("헤더작업");
+  const header = document.querySelector(".header");
+  let scTop = 0;
+  window.addEventListener("scroll", function () {
+    scTop = document.documentElement.scrollTop;
+    if (scTop > 0) {
+      header.classList.add("active");
+    } else {
+      header.classList.remove("active");
+    }
+  });
+}
--- js/sidemenu.js
+++ js/sidemenu.js
@@ -3,15 +3,19 @@
   const langBt = document.getElementById("lang-bt");
   const langList = document.getElementById("lang-list");
 
-  const toggleListArr = [langList];
-  const toggleBtArr = [langBt];
-  
-  function listToggle(bt,list){
-    bt.style.display = "block";
-    
-  }
+  // const toggleListArr = [langList];
+  // const toggleBtArr = [langBt];
 
+  // a 태그 클릭해도 이벤트 전달 막기
+  // toggleListArr.forEach(function (item) {
+  //   item.addEventListener("click", function (event) {
+  //     event.stopPropagation();
+  //   });
+  // });
   langBt.addEventListener("click", function () {
     langList.style.display = "block";
   });
+  //   langBt.addEventListener("click", function () {
+  //     langList.style.display = "none";
+  //   });
 }
